6083668872ff96e75293cc56535328e27bed3f5e
avivpolak <avivp0437@gmail.com>
2024-01-24 13:16:47 +0200

    finishing formating

--- src/base/base.entity.ts
+++ src/base/base.entity.ts
@@ -1,11 +1,11 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 //import { ApiModelProperty, ApiModelPropertyOptional } from '@nestjs/swagger';
 
-export class BaseEntity{
+export class BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  constructor(entity:Partial<BaseEntity>){
-    Object.assign(this,entity)
+  constructor(entity: Partial<BaseEntity>) {
+    Object.assign(this, entity);
   }
 }
